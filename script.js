c8ba1a9e1e163195ae968658a2ab125d3a0bbc0d
dwaynethebroc <127725696+dwaynethebroc@users.noreply.github.com>
2023-06-15 18:26:47 +0900

    final push

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+/*
+Step 1) function getComputerChoice returns Rock, Paper, Scissors
+
+		randomly generate a number between 1-100
+		if 1-33 -> rock
+		if 34-66 -> paper
+		if 67-99 -> scissors 
+		if 100 -> re-do random number
+
+		store string value of rock, paper, or scissors, in computerSelection variable  
+
+Step 2) function called playerSelection (User enters an input)
+		string text is converted to all lowercase 
+		string text, first letter is capitalized
+		function makes sure only rock, paper, or scissors is entered
+		if anything else, player re-enters input until one of 3 options (rock, paper, scissors) is entered
+		store value in variable playerSelection 
+
+Step 3) create function singleRoundRPS 
+		
+		call playerSelection and computerSelection in singleRoundRPS
+		Logic (call variables in print message, not hardcode answers) -> 
+
+		if playerSelection = ‘Rock’ and computerSelection = ‘Scissors’ - print “You win! Rock beats Scissors” 
+		if playerSelection = ‘Rock’ and computerSelection = ‘Rock’ - print “You tie! Play another round” 
+		if playerSelection = ‘Rock’ and computerSelection = ‘Paper’ - print “You lose! Paper beats Rock” 
+
+		if playerSelection = ‘Scissors’ and computerSelection = ‘Paper’ - print “You win! Scissors beats Paper” 
+		if playerSelection = ‘Scissors’ and computerSelection = ‘Scissors’ - print “You tie! Play another round” 
+		if playerSelection = ‘Rock’ and computerSelection = ‘Paper’ - print “You lose! Paper beats Rock” 
+
+		if playerSelection = ‘Paper’ and computerSelection = ‘Rock’- print “You win! Paper beats Rock” 
+		if playerSelection = ‘Paper’ and computerSelection = ‘Paper’ - print “You tie! Play another round” 
+		if playerSelection = ‘Paper’ and computerSelection = ‘Scissors’ - print “You lose! Scissors beats Paper” 
+
+
+Step 4) call function game() 
+
+		Insert function singleRoundRPS() five times 
+*/
+
 let computerSelection; 
 let playerSelection;
 let winsP = 0;
@@ -58,47 +99,56 @@ function singleRoundRPS(computerSelection, playerSelection){
     computerSelection = getPlayerChoice();
 
     console.log(playerSelection + ' ' + '= player');
-    console.log(computerSelection + ' ' + 'computer');
+    console.log(computerSelection + ' ' + '= computer');
 
     /* win logic */
     if (playerSelection === 'Rock' && computerSelection === 'Scissors'){
         window.alert(`You have selected '${playerSelection}' -> the computer has chosen '${computerSelection}'\n \n You have won -> '${playerSelection}' beats '${computerSelection}'`);
+        console.log(`winner: player`)
         return winsP = winsP + 1; 
     }
     else if (playerSelection === 'Paper' && computerSelection === 'Rock'){
         window.alert(`You have selected '${playerSelection}' -> the computer has chosen '${computerSelection}'\n \n You have won -> '${playerSelection}' beats '${computerSelection}'`); 
+        console.log(`winner: player`)
         return winsP = winsP + 1;
     }
     else if (playerSelection === 'Scissors' && computerSelection === 'Paper'){
         window.alert(`You have selected '${playerSelection}' -> the computer has chosen '${computerSelection}'\n \n You have won -> '${playerSelection}' beats '${computerSelection}'`); 
+        console.log(`winner: player`)
         return winsP = winsP + 1;
     }
     
     /* tie logic */
     else if (playerSelection === 'Rock' && computerSelection === 'Rock'){
         window.alert(`You have selected '${playerSelection}' -> the computer has chosen '${computerSelection}'\n \n You have tied -> '${playerSelection}' does not beat '${computerSelection}'`);
+        console.log(`winner: nobody`)
         return tiesP = tiesP + 1; 
     }
     else if (playerSelection === 'Paper' && computerSelection === 'Paper'){
         window.alert(`You have selected '${playerSelection}' -> the computer has chosen '${computerSelection}'\n \n You have tied -> '${playerSelection}' does not beat '${computerSelection}'`);
+        console.log(`winner: nobody`)
         return tiesP = tiesP + 1;  
     }
     else if (playerSelection === 'Scissors' && computerSelection === 'Scissors'){
         window.alert(`You have selected '${playerSelection}' -> the computer has chosen '${computerSelection}'\n \n You have tied -> '${playerSelection}' does not beat '${computerSelection}'`);
+        console.log(`winner: nobody`)
         return tiesP = tiesP + 1;  
     }
 
     /* lose logic */
     else if (playerSelection === 'Rock' && computerSelection === 'Paper'){
         window.alert(`You have selected '${playerSelection}' -> the computer has chosen '${computerSelection}'\n \n You have lost -> '${computerSelection}' beats '${playerSelection}'`);
+        console.log(`winner: computer`)
         return lossesP = lossesP + 1;  
     }
     else if (playerSelection === 'Paper' && computerSelection === 'Scissors'){
         window.alert(`You have selected '${playerSelection}' -> the computer has chosen '${computerSelection}'\n \n You have lost -> '${computerSelection}' beats '${playerSelection}'`);
+        console.log(`winner: computer`)
         return lossesP = lossesP + 1;  
     }
     else if (playerSelection === 'Scissors' && computerSelection === 'Rock'){
         window.alert(`You have selected '${playerSelection}' -> the computer has chosen '${computerSelection}'\n \n You have lost -> '${computerSelection}' beats '${playerSelection}'`);
+        console.log(`winner: computer`)
         return lossesP = lossesP + 1;  
     }
 }
@@ -110,6 +160,7 @@ function game() {
     singleRoundRPS(); 
     singleRoundRPS(); 
     
+    console.log(`You won ${winsP} games \n Tied ${tiesP} games \n Lost ${lossesP} games \n \n The computer won ${0+lossesP} \n The computer tied ${0+tiesP} \n The computer lost ${0 + winsP}`)
     window.alert(`You won ${winsP} games \n Tied ${tiesP} games \n Lost ${lossesP} games \n \n The computer won ${0+lossesP} \n The computer tied ${0+tiesP} \n The computer lost ${0 + winsP}`)
 
     if (winsP >= 3) {
@@ -144,43 +195,4 @@ function game() {
 game();
 
 
-/*
-Step 1) function getComputerChoice returns Rock, Paper, Scissors
 
-		randomly generate a number between 1-100
-		if 1-33 -> rock
-		if 34-66 -> paper
-		if 67-99 -> scissors 
-		if 100 -> re-do random number
-
-		store string value of rock, paper, or scissors, in computerSelection variable  
-
-Step 2) function called playerSelection (User enters an input)
-		string text is converted to all lowercase 
-		string text, first letter is capitalized
-		function makes sure only rock, paper, or scissors is entered
-		if anything else, player re-enters input until one of 3 options (rock, paper, scissors) is entered
-		store value in variable playerSelection 
-
-Step 3) run function singleRoundRPS 
-		
-		call playerSelection and computerSelection in singleRoundRPS
-		Logic (call variables in print message, not hardcode answers) -> 
-
-		if playerSelection = ‘Rock’ and computerSelection = ‘Scissors’ - print “You win! Rock beats Scissors” 
-		if playerSelection = ‘Rock’ and computerSelection = ‘Rock’ - print “You tie! Play another round” 
-		if playerSelection = ‘Rock’ and computerSelection = ‘Paper’ - print “You lose! Paper beats Rock” 
-
-		if playerSelection = ‘Scissors’ and computerSelection = ‘Paper’ - print “You win! Scissors beats Paper” 
-		if playerSelection = ‘Scissors’ and computerSelection = ‘Scissors’ - print “You tie! Play another round” 
-		if playerSelection = ‘Rock’ and computerSelection = ‘Paper’ - print “You lose! Paper beats Rock” 
-
-		if playerSelection = ‘Paper’ and computerSelection = ‘Rock’- print “You win! Paper beats Rock” 
-		if playerSelection = ‘Paper’ and computerSelection = ‘Paper’ - print “You tie! Play another round” 
-		if playerSelection = ‘Paper’ and computerSelection = ‘Scissors’ - print “You lose! Scissors beats Paper” 
-
-
-Step 4) call function game() 
-
-		Insert function singleRoundRPS() five times 
-*/
